92d49190ddd23d3717437ebeba4a8ff9c0ace734
adambenaceur <benaceur.adam@gmail.com>
2023-06-29 12:49:23 -0600

    Initialize the SDK and contract on project

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, useContract } from "@thirdweb-dev/react";
 import "./styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "goerli";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -19,6 +19,11 @@ root.render(
     </ThirdwebProvider>
   </React.StrictMode>
 );
+
+function Component() {
+  const {contract, isLoading } = useContract("0x750685ea3918804c736660DC7FdAe31ed8428E49");
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
