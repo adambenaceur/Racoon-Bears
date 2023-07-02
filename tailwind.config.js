b622cdd029254fd65b167a249110da85fae61732
Adam <adam@Adams-MacBook-Air.local>
2023-07-01 20:12:04 -0600

    adding color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        coiny: ['Coiny', ... defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        "brand-purple": "var(--clr-purple)",
+        "brand-pink": "var(--clr-pink)",
+        "brand-yellow": "var(--clr-yellow)",
+        "brand-blue": "var(--clr-blue)",
+        "brand-green": "var(--clr-green)",
+      }
+    },
   },
   plugins: [],
 }
