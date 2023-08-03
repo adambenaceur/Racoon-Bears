da0c72a0082520118363857304211ed4dcb23e8a
adambenaceur <benaceur.adam@gmail.com>
2023-08-02 23:26:11 -0600

    added ipsum lorem text'

--- src/App.tsx
+++ src/App.tsx
@@ -72,6 +72,7 @@ const Home = () => {
        <div className=" h-full w-full container max-w-5xl mx-auto flex flex-col items-center pt-4"> {/* about */}
         <div> {/* about */}
           <h1>About Racoon Bears</h1>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
         </div>
       </div>
     </div>
