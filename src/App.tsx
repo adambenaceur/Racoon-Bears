25aacff166e521e794afd72d82cba0411c8912f3
adambenaceur <benaceur.adam@gmail.com>
2023-08-16 03:32:15 -0600

    added minting page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import logo from '../images/RacoonBearPreview.png';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import Minting from './pages/Minting';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/minting" element={<Minting />} />
       </Routes>
     </BrowserRouter>
   );
